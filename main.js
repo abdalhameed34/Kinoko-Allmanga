b83675adfbdc50c8f203e59601d11d800da15bfe
abdalhameed34 <62402305+abdalhameed34@users.noreply.github.com>
2026-03-06 06:38:26 +0400

    Update main.js

--- main.js
+++ main.js
@@ -1,21 +1,26 @@
-const { ParsedCollection } = require('./collection');
-
 class MainController extends Controller {
     load(data) {
-        // 1. Initialize the scraper with the AllManga URL from the tab
-        this.collection = ParsedCollection.new(data);
-        
-        // 2. Find our new bulletproof list
+        // 1. Find the list
         this.list = this.findElement('manga_list');
         
-        // 3. Bind the scraper to the list (This triggers the network request)
-        if (this.list) {
-            this.list.setCollection(this.collection);
+        // 2. Create an offline, fake collection
+        let mockCollection = glib.Collection.new();
+        
+        mockCollection.reload = function(_, cb) {
+            let item = glib.DataItem.new();
+            item.title = "THE UI IS SAFE!";
+            item.subtitle = "The HeadlessWebView is the killer.";
+            // A safe, standard test image
+            item.picture = "https://via.placeholder.com/80x110"; 
             
-            // Make the manga clickable
-            this.list.onItemClick = (item) => {
-                this.openBook(item);
-            };
+            this.setData([item]);
+            cb.apply(null);
+            return true;
+        };
+        
+        // 3. Bind the safe data to the UI
+        if (this.list) {
+            this.list.setCollection(mockCollection);
         }
     }
 }
