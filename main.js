dd785139137ff68342b36edb0a5207560f461638
abdalhameed34 <62402305+abdalhameed34@users.noreply.github.com>
2026-03-06 06:13:19 +0400

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,25 @@
 class MainController extends Controller {
+    
     load(data) {
-        // Absolutely nothing here. No scraping, no lists, no data bindings.
+        // 1. Find the list
+        this.list = this.findElement('manga_list');
+        
+        // 2. Create a fake collection directly inside this file
+        let mockCollection = glib.Collection.new();
+        
+        // 3. Override the reload function to instantly provide fake data
+        mockCollection.reload = function(_, cb) {
+            let item = glib.DataItem.new();
+            item.title = "IT FINALLY WORKS!";
+            item.subtitle = "The UI is rendering perfectly.";
+            
+            this.setData([item]);
+            cb.apply(null);
+            return true;
+        };
+        
+        // 4. Bind it to the list
+        this.list.setCollection(mockCollection);
     }
 }
 
