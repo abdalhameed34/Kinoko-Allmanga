2a5d24327a9407f859ea68a20d592e187ec70f31
abdalhameed34 <62402305+abdalhameed34@users.noreply.github.com>
2026-03-06 06:04:40 +0400

    Update main.js

--- main.js
+++ main.js
@@ -1,20 +1,6 @@
-const { ParsedCollection } = require('./collection');
-
 class MainController extends Controller {
     load(data) {
-        // 1. Initialize the scraper with the URL from the tab
-        this.collection = ParsedCollection.new(data);
-        
-        // 2. Find our simple text list
-        this.list = this.findElement('manga_list');
-        
-        // 3. Bind the data (This triggers the network request)
-        this.list.setCollection(this.collection);
-        
-        // 4. Make the text items clickable
-        this.list.onItemClick = (item) => {
-            this.openBook(item);
-        };
+        // Absolutely nothing here. No scraping, no lists, no data bindings.
     }
 }
 
