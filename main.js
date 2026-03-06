09889962874ae3ac3d28f37ba06f9e68339ce14e
abdalhameed34 <62402305+abdalhameed34@users.noreply.github.com>
2026-03-06 05:33:31 +0400

    Update main.js

--- main.js
+++ main.js
@@ -1,28 +1,8 @@
-// Import the collection classes we adapted earlier
-const { ParsedCollection } = require('./collection');
-
 class MainController extends Controller {
-    
-    // 'data' contains the tab info, e.g., { title: "All Manga", url: "https://allmanga.to/manga?cty=ALL" }
     load(data) {
-        // Initialize our AllManga scraper with the tab's URL
-        this.collection = ParsedCollection.new(data);
-        
-        // Find the list element from main.xml
+        // Find the list, but don't attach any data to it yet.
+        // We just want to see if the tab can open without killing the app.
         this.list = this.findElement('manga_list');
-        
-        // Bind the data to the UI. Kinoko will automatically call this.collection.reload()
-        this.list.setCollection(this.collection);
-        
-        // Set an action for when a user taps a manga cover
-        this.list.onItemClick = (item) => {
-            // Open the detail page for that specific manga
-            this.openBook(item);
-        };
-    }
-
-    unload() {
-        // Clean up when leaving the tab
     }
 }
 
