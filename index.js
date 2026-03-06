40b13bc7e50e2fbc5e77c5853d0d16cf903bb502
abdalhameed34 <62402305+abdalhameed34@users.noreply.github.com>
2026-03-06 06:19:18 +0400

    Update index.js

--- index.js
+++ index.js
@@ -1,25 +1,14 @@
 class IndexController extends Controller {
-    
     load() {
-        // 1. Find the list directly on the index page
-        this.list = this.findElement('manga_list');
-        
-        // 2. Create the fake data collection
-        let mockCollection = glib.Collection.new();
-        
-        mockCollection.reload = function(_, cb) {
-            let item = glib.DataItem.new();
-            item.title = "ROUTING WAS THE PROBLEM!";
-            item.subtitle = "The widget tag was silently failing the whole time.";
-            
-            this.setData([item]);
-            cb.apply(null);
-            return true;
+        this.data = {
+            tabs: [
+                {
+                    "title": "UI Test",
+                    "id": "test",
+                    "url": "https://allmanga.to/manga?cty=ALL"
+                }
+            ]
         };
-        
-        // 3. Bind it
-        this.list.setCollection(mockCollection);
     }
 }
-
 module.exports = IndexController;
