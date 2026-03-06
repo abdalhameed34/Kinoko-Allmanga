f6aba2a400f1d3bb9b3c34809c93d41920d5c33b
abdalhameed34 <62402305+abdalhameed34@users.noreply.github.com>
2026-03-06 06:03:14 +0400

    Update main.js

--- main.js
+++ main.js
@@ -1,8 +1,20 @@
+const { ParsedCollection } = require('./collection');
+
 class MainController extends Controller {
     load(data) {
-        // Find the list, but don't attach any data to it yet.
-        // We just want to see if the tab can open without killing the app.
+        // 1. Initialize the scraper with the URL from the tab
+        this.collection = ParsedCollection.new(data);
+        
+        // 2. Find our simple text list
         this.list = this.findElement('manga_list');
+        
+        // 3. Bind the data (This triggers the network request)
+        this.list.setCollection(this.collection);
+        
+        // 4. Make the text items clickable
+        this.list.onItemClick = (item) => {
+            this.openBook(item);
+        };
     }
 }
 
