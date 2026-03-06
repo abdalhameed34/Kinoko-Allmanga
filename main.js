e6ed604a43ac707e130752936e66a79337bd1745
abdalhameed34 <62402305+abdalhameed34@users.noreply.github.com>
2026-03-06 07:06:40 +0400

    Update main.js

--- main.js
+++ main.js
@@ -1,25 +1,27 @@
+class MockCollection extends glib.Collection {
+    constructor(data) {
+        super(data);
+    }
+
+    reload(_, cb) {
+        let item = glib.DataItem.new();
+        item.title = "THE DATA BINDING WORKS!";
+        
+        this.setData([item]);
+        cb();
+        return true;
+    }
+}
+
 class MainController extends Controller {
     load(data) {
-        // 1. Find the explicitly sized list
+        // Find the list (which should work now that it's inside a column)
         this.list = this.findElement('manga_list');
         
-        // 2. Use the native glib object directly (no custom classes)
-        let mockCollection = glib.Collection.new();
+        this.collection = new MockCollection(data);
         
-        // 3. Override its internal reload function
-        mockCollection.reload = function(page, cb) {
-            let item = glib.DataItem.new();
-            item.title = "THE LIST IS ALIVE!";
-            item.subtitle = "We fixed the silent JS error and layout collapse.";
-            
-            this.setData([item]);
-            cb(); 
-            return true;
-        };
-        
-        // 4. Bind it
         if (this.list) {
-            this.list.setCollection(mockCollection);
+            this.list.setCollection(this.collection);
         }
     }
 }
