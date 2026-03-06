65d67073a02ac5c405c43f7e5a66adb941bb585c
abdalhameed34 <62402305+abdalhameed34@users.noreply.github.com>
2026-03-06 06:37:06 +0400

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,22 @@
+const { ParsedCollection } = require('./collection');
+
 class MainController extends Controller {
     load(data) {
-        // Just let the XML render the static text
+        // 1. Initialize the scraper with the AllManga URL from the tab
+        this.collection = ParsedCollection.new(data);
+        
+        // 2. Find our new bulletproof list
+        this.list = this.findElement('manga_list');
+        
+        // 3. Bind the scraper to the list (This triggers the network request)
+        if (this.list) {
+            this.list.setCollection(this.collection);
+            
+            // Make the manga clickable
+            this.list.onItemClick = (item) => {
+                this.openBook(item);
+            };
+        }
     }
 }
 
