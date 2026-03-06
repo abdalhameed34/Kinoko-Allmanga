ee185de40f562bf73b9999201fe4623910c8d876
abdalhameed34 <62402305+abdalhameed34@users.noreply.github.com>
2026-03-06 06:35:37 +0400

    Update main.js

--- main.js
+++ main.js
@@ -1,20 +1,7 @@
 class MainController extends Controller {
     load(data) {
-        this.list = this.findElement('manga_list');
-        
-        let mockCollection = glib.Collection.new();
-        mockCollection.reload = function(_, cb) {
-            let item = glib.DataItem.new();
-            item.title = "THE WIDGET BRIDGE IS ALIVE!";
-            
-            this.setData([item]);
-            cb.apply(null);
-            return true;
-        };
-        
-        if (this.list) {
-            this.list.setCollection(mockCollection);
-        }
+        // Just let the XML render the static text
     }
 }
+
 module.exports = MainController;
