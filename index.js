385a0e19a3a6f01028dfbfe179ec892ef53a6a9d
abdalhameed34 <62402305+abdalhameed34@users.noreply.github.com>
2026-03-06 06:28:42 +0400

    Update index.js

--- index.js
+++ index.js
@@ -1,25 +1,14 @@
 class IndexController extends Controller {
     load() {
-        // 1. Find the list directly inside our new tab
-        this.list = this.findElement('manga_list');
-        
-        // 2. Create an offline, fake collection
-        let mockCollection = glib.Collection.new();
-        mockCollection.reload = function(_, cb) {
-            let item = glib.DataItem.new();
-            item.title = "THE LIST SURVIVED!";
-            item.subtitle = "We can safely put lists inside tabs.";
-            
-            this.setData([item]);
-            cb.apply(null);
-            return true;
+        this.data = {
+            tabs: [
+                {
+                    "title": "AllManga",
+                    "id": "latest",
+                    "url": "https://allmanga.to/search-manga?cty=ALL"
+                }
+            ]
         };
-        
-        // 3. Bind the data
-        if (this.list) {
-            this.list.setCollection(mockCollection);
-        }
     }
 }
-
 module.exports = IndexController;
