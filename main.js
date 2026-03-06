1f4b478eaae386111896b91fdd76410a04a2b52c
abdalhameed34 <62402305+abdalhameed34@users.noreply.github.com>
2026-03-06 06:56:45 +0400

    Update main.js

--- main.js
+++ main.js
@@ -1,33 +1,15 @@
-// 1. We formally declare a strict Collection class
-class MockCollection extends glib.Collection {
-    constructor(data) {
-        super(data);
-    }
-
-    // 2. We use the built-in override method properly
-    reload(_, cb) {
-        let item = glib.DataItem.new();
-        item.title = "CHALLENGE COMPLETED!";
-        item.subtitle = "The class structure was the missing key.";
-        
-        // A bright red placeholder image so it's impossible to miss
-        item.picture = "https://via.placeholder.com/150/ff0000/ffffff?text=SUCCESS"; 
-        
-        this.setData([item]);
-        cb.apply(null);
-        return true;
-    }
-}
+const { ParsedCollection } = require('./collection');
 
 class MainController extends Controller {
     load(data) {
+        this.collection = ParsedCollection.new(data);
         this.list = this.findElement('manga_list');
-        
-        // 3. Initialize our formal class
-        this.collection = MockCollection.new(data);
         
         if (this.list) {
             this.list.setCollection(this.collection);
+            this.list.onItemClick = (item) => {
+                this.openBook(item);
+            };
         }
     }
 }
