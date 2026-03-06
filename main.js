d6eb782b23cf84350a5bfacf28598d8f1b736267
abdalhameed34 <62402305+abdalhameed34@users.noreply.github.com>
2026-03-06 06:45:26 +0400

    Update main.js

--- main.js
+++ main.js
@@ -1,26 +1,33 @@
+// 1. We formally declare a strict Collection class
+class MockCollection extends glib.Collection {
+    constructor(data) {
+        super(data);
+    }
+
+    // 2. We use the built-in override method properly
+    reload(_, cb) {
+        let item = glib.DataItem.new();
+        item.title = "CHALLENGE COMPLETED!";
+        item.subtitle = "The class structure was the missing key.";
+        
+        // A bright red placeholder image so it's impossible to miss
+        item.picture = "https://via.placeholder.com/150/ff0000/ffffff?text=SUCCESS"; 
+        
+        this.setData([item]);
+        cb.apply(null);
+        return true;
+    }
+}
+
 class MainController extends Controller {
     load(data) {
-        // 1. Find the list
         this.list = this.findElement('manga_list');
         
-        // 2. Create an offline, fake collection
-        let mockCollection = glib.Collection.new();
+        // 3. Initialize our formal class
+        this.collection = MockCollection.new(data);
         
-        mockCollection.reload = function(_, cb) {
-            let item = glib.DataItem.new();
-            item.title = "THE UI IS SAFE!";
-            item.subtitle = "The HeadlessWebView is the killer.";
-            // A safe, standard test image
-            item.picture = "https://via.placeholder.com/80x110"; 
-            
-            this.setData([item]);
-            cb.apply(null);
-            return true;
-        };
-        
-        // 3. Bind the safe data to the UI
         if (this.list) {
-            this.list.setCollection(mockCollection);
+            this.list.setCollection(this.collection);
         }
     }
 }
