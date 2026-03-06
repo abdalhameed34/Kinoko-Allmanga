b1b9436cb9ddb453c3a7b4889583fee5d76d6b2a
abdalhameed34 <62402305+abdalhameed34@users.noreply.github.com>
2026-03-06 06:25:05 +0400

    Update index.js

--- index.js
+++ index.js
@@ -1,14 +1,23 @@
+const { ParsedCollection } = require('./collection');
+
 class IndexController extends Controller {
     load() {
-        this.data = {
-            tabs: [
-                {
-                    "title": "UI Test",
-                    "id": "test",
-                    "url": "https://allmanga.to/manga?cty=ALL"
-                }
-            ]
+        // 1. Create a data object with the AllManga Latest Updates URL
+        let targetData = glib.DataItem.new();
+        targetData.link = "https://allmanga.to/search-manga?cty=ALL";
+        
+        // 2. Initialize your scraper from collection.js
+        this.collection = ParsedCollection.new(targetData);
+        
+        // 3. Find the list and bind the scraper to it
+        this.list = this.findElement('manga_list');
+        this.list.setCollection(this.collection);
+        
+        // 4. Make the items clickable
+        this.list.onItemClick = (item) => {
+            this.openBook(item);
         };
     }
 }
+
 module.exports = IndexController;
